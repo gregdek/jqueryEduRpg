1c836d556484de2b0321100f42eb0556a88a3712
Greg DeKoenigsberg <greg.dekoenigsberg@gmail.com>
2010-05-22 17:02:24 -0400

    Maps are now in a separate file, and multiple maps can be specified. Not yet possible to move between them, but that will be soon.

---
+++ jQRPG_files/jqrpg-maps.js
@@ -0,0 +1,39 @@
+mapset = new Object();
+
+mapset.height = new Array();
+mapset.width= new Array();
+mapset.picture = new Array();
+
+mapset.height[0] = 16;
+mapset.width[0] = 16;
+
+mapset.picture[0] = 
+ 'xxxxxxxxxxxxxxxx' +
+ 'x______________x' +
+ 'x______________x' +
+ 'x__111111111___x' +
+ 'x______________x' +
+ 'x______________x' +
+ 'x______________x' +
+ 'x___2___0______x' +
+ 'x______________x' +
+ 'x___2_______2__x' +
+ 'x___________2__x' +
+ 'x___2_______2__x' +
+ 'x______333_____x' +
+ 'x_____33333____x' +
+ 'x______333_____x' +
+ 'xxxxxxxxxxxxxxxx' ;
+
+mapset.height[1] = 8;
+mapset.width[1] = 8;
+
+mapset.picture[1] = 
+ '11111111' + 
+ '1______1' +
+ '1______1' +
+ '1______1' +
+ '1__0___1' +
+ '1______1' +
+ '1______1' +
+ '11111111' ;
